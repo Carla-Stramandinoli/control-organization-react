9868e049aa6f89243ea2bb9b541cc0a62f807e5a
Carla Stramandinoli <carla.stram@gmail.com>
2023-08-23 16:48:46 -0300

    Agrego pagina expenses

--- src/components/addExpensesComponent/addExpenses.jsx
+++ src/components/addExpensesComponent/addExpenses.jsx
@@ -1,8 +1,10 @@
 import styled from "@emotion/styled";
 import {
+  Button,
   Card,
   Container,
   FormLabel,
+  Grid,
   Input,
 } from "@mui/material";
 import React from "react";
@@ -11,14 +13,20 @@ function AddExpenses() {
   return (
     <Container>
       <CustomCard>
-        <FormLabel>Descripcion del gasto:</FormLabel>
-        <Input
-          type="text"
-        />
-        <FormLabel>Precio:</FormLabel>
-        <Input
-          type="interger"
-        />
+        <Grid p={1} >
+          <FormLabel>Descripcion del gasto:</FormLabel>
+          <Input
+            type="text"
+          />
+          <FormLabel>Precio:</FormLabel>
+          <Input
+            type="number"
+          />
+        </Grid>
+
+        <Grid container item xs={12} sx={{ justifyContent: "center" }} p={1}>
+          <CustomButtonAdd>Agregar</CustomButtonAdd>
+        </Grid>
       </CustomCard>
     </Container>
   );
@@ -33,4 +41,14 @@ const CustomCard = styled(Card)({
   marginTop: "2%",
   marginLeft: "22%",
   padding: "8px"
+});
+
+const CustomButtonAdd = styled(Button)({
+  backgroundColor: "#AC8085",
+  color: "white",
+  padding: "10px",
+  margin: "6px",
+  border: "1px solid black",
+  borderRadius: "8px",
+  fontSize: "15px",
 });
