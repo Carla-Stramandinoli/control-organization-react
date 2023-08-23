862d8ae62cd91ebf8e4ae53438ccb4c6563fe36b
Carla Stramandinoli <carla.stram@gmail.com>
2023-08-23 17:35:22 -0300

    Agrego pagina expenses

--- src/components/addExpensesComponent/addExpenses.jsx
+++ src/components/addExpensesComponent/addExpenses.jsx
@@ -8,8 +8,40 @@ import {
   Input,
 } from "@mui/material";
 import React from "react";
+import Swal from "sweetalert2";
 
 function AddExpenses() {
+  const [expense, setExpense] = React.useState("");
+  const [price, setPrice] = React.useState(0);
+
+  const handleExpense = (e) => {
+    setExpense(e.target.value);
+  };
+
+  const handlePrice = (e) => {
+    const inputValue = parseInt(e.target.value, 10);
+    if (!isNaN(inputValue) && inputValue >= 0 && inputValue <= 1000) {
+      setPrice(inputValue);
+    }
+  };
+
+  const addExpense = () => {
+    if (
+      expense === "" ||
+      price === ""
+    ) {
+      Swal.fire("Todos los campos deben estar completos!");
+      return;
+    }
+    const newProduct = {
+      expense,
+      price,
+    };
+    // sendProdLoad(newProduct);
+    setExpense("");
+    setPrice(0);
+    console.log(newProduct);
+  };
   return (
     <Container>
       <CustomCard>
@@ -17,15 +49,19 @@ function AddExpenses() {
           <FormLabel>Descripcion del gasto:</FormLabel>
           <Input
             type="text"
+            value={expense}
+            onChange={handleExpense}
           />
           <FormLabel>Precio:</FormLabel>
           <Input
             type="number"
+            value={price}
+            onChange={handlePrice}
           />
         </Grid>
 
         <Grid container item xs={12} sx={{ justifyContent: "center" }} p={1}>
-          <CustomButtonAdd>Agregar</CustomButtonAdd>
+          <CustomButtonAdd onClick={addExpense}>Agregar</CustomButtonAdd>
         </Grid>
       </CustomCard>
     </Container>
@@ -40,7 +76,7 @@ const CustomCard = styled(Card)({
   maxWidth: "50%",
   marginTop: "2%",
   marginLeft: "22%",
-  padding: "8px"
+  padding: "8px",
 });
 
 const CustomButtonAdd = styled(Button)({
@@ -51,4 +87,7 @@ const CustomButtonAdd = styled(Button)({
   border: "1px solid black",
   borderRadius: "8px",
   fontSize: "15px",
+  "&:hover": {
+    backgroundColor: "#B98A7A",
+  },
 });
